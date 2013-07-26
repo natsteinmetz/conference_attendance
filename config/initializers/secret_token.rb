8bea16eacd5529be09f54099ea60585decf086d0
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-07-26 16:03:47 -0700

    Created conference attendance app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenceAttendance::Application.config.secret_token = '6bbeae7b7f45dd54301f9de8a1caae7fdbd8b18be1040b78edfbc91b963ed4ed04e167cc94b0a3a57f25144bd2ee27d50862a3c902fa4b1c2ebaa0ddd060d8d7'
